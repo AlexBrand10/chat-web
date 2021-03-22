60f1ccf80082abe372e9fb77a8c46d0ce5c907e0
AlexBrand10 <brandongarciam@gmail.com>
2021-03-22 08:32:15 -0600

    master
    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http, {
+    cors: {
+        origins: ['http://localhost:4200']
+    }
+});
+
+app.get('/', (req, res) => res.send('hello!'));
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('message', (msg) => {
+        console.log(msg);
+        socket.broadcast.emit('message-broadcast', msg);
+    });
+});
+
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
